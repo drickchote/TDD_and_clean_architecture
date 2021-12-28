3168cf6ab747887ff0f3a89127a80cfa47d0e07e
Marcos <mdrick19@gmail.com>
2021-12-28 10:51:45 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/registerUserOnMailingList/registerUseOnMailingList.spec.ts
@@ -0,0 +1,7 @@
+import { userData } from "./userData"
+
+describe('Register user on mailing list use case', () => {
+    test('should add user with complete data to mailing list', async () => {
+        const users: userData[] = []
+    })
+})
